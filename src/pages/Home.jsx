e0057e20622463b0f72827265791189d31d38e3b
yurico <kom.jet.aime.9@gmail.com>
2023-08-28 21:33:06 +0900

    update: supported responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,37 +18,56 @@ const Home = () => {
 
   return (
     <>
+      <h1 className="title">宝塚おとめWeb版</h1>
       <div className="topContainer">
-        <div className="topArea">
-          <div className="topDescription" style={{ fontWeight: "600" }}>
-            宝塚おとめWeb版
-          </div>
-          <div className="topDescription pc-only">
-            <ul className="topLinkList">
-              <li>
-                <Link to="/otome/star">
-                  {iFaCaretRight}
-                  全生徒一覧
-                </Link>
-              </li>
-            </ul>
-            <ul className="topLinkList">{linkList}</ul>
-          </div>
-          <div className="topDescription sp-only">
-            <ul className="topLinkList">
-              <li>
-                <Link to="/otome/star">
-                  {iFaCaretRight}
-                  全生徒一覧
-                </Link>
-              </li>
-              {linkList}
-            </ul>
-          </div>
+        <div className="linkContainer">
+          <ul className="linkList">
+            <li>
+              <Link to="/otome/star">
+                {iFaCaretRight}
+                全生徒一覧
+              </Link>
+            </li>
+          </ul>
+          <ul className="linkList">{linkList}</ul>
         </div>
       </div>
     </>
   );
+
+  // return (
+  //   <>
+  //     <div className="topContainer">
+  //       <div className="topArea">
+  //         <div className="topDescription" style={{ fontWeight: "600" }}>
+  //           宝塚おとめWeb版
+  //         </div>
+  //         <div className="topDescription pc-only">
+  //           <ul className="topLinkList">
+  //             <li>
+  //               <Link to="/otome/star">
+  //                 {iFaCaretRight}
+  //                 全生徒一覧
+  //               </Link>
+  //             </li>
+  //           </ul>
+  //           <ul className="topLinkList">{linkList}</ul>
+  //         </div>
+  //         <div className="topDescription sp-only">
+  //           <ul className="topLinkList">
+  //             <li>
+  //               <Link to="/otome/star">
+  //                 {iFaCaretRight}
+  //                 全生徒一覧
+  //               </Link>
+  //             </li>
+  //             {linkList}
+  //           </ul>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default Home;
